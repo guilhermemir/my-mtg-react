cbf610e017da2a4722a168e712e2489fa408abb9
Guilherme Mir <guilherme.mir@gmail.com>
2022-09-16 03:37:06 -0300

    Search bar + Card style update

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,17 +1,16 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 function Card(props) {
-  const scryfallId = props.scryfallId
+  const scryfallId = props.scryfallId;
 
-  const [name, setName] = useState("")
-  const [imageUrl, setImageUrl] = useState("")
+  const [name, setName] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
 
-  fetch(`https://api.scryfall.com/cards/${scryfallId}`)
-    .then(response => {
-      response.json().then(data => {
-        setName(data.name);
-        setImageUrl(data.image_uris.large);
-        /*
+  fetch(`https://api.scryfall.com/cards/${scryfallId}`).then((response) => {
+    response.json().then((data) => {
+      setName(data.name);
+      setImageUrl(data.image_uris.large);
+      /*
           data.image_uris.large
           data.image_uris.normal
           data.image_uris.small
@@ -20,39 +19,63 @@ function Card(props) {
           data.card_faces[0].image_uris.small
           "https://via.placeholder.com/672x936"
         */
-      });
+    });
   });
 
   return (
-        <div className="bg-white p-3">
-          <img className="w-w-px" src={imageUrl} />
-          <ul className="mt-3 flex flex-wrap">
-            <li className="mr-auto">
-              <a href="#" className="flex text-gray-400 hover:text-gray-600">
-                {name}
-              </a>
-            </li>
-            <li className="mr-2">
-              <a href="#" className="flex text-gray-400 hover:text-gray-600">
-                +
-              </a>
-            </li>
-            <li className="mr-2 text-gray-400 hover:text-gray-600">
-                Qty
-            </li>
-            <li className="mr-2">
-              <a href="#" className="flex text-gray-400 hover:text-gray-600">
-                -
-              </a>
-            </li>
-            <li>
-              <a href="#" className="flex text-gray-400 hover:text-gray-600">
-                Delete
-              </a>
-            </li>
-          </ul>
-        </div>
+    <div className="w-80 bg-white rounded-xl p-3 shadow-xl">
+      <img className="" src={imageUrl} alt={name} title={name} />
+      <ul className="flex flex-wrap gap-1 mt-2 items-center">
+        <li className="mr-auto">
+          <a href="#" className="hover:text-gray-600">
+            {name}
+          </a>
+        </li>
+        <li>
+          <button
+            type="button"
+            class="border border-gray-700 bg-gray-700 text-white font-semibold rounded-md px-2 py-1 transition duration-300 ease select-none hover:bg-gray-800 focus:outline-none focus:shadow-outline"
+          >
+            +
+          </button>
+        </li>
+        <li>
+          <input
+            type="number"
+            name=""
+            id=""
+            value={5}
+            class="border border-gray-200 bg-gray-200 text-gray-700 w-12 rounded-md px-2 py-1 transition duration-300 ease select-none hover:bg-gray-300 focus:outline-none focus:shadow-outline"
+          />
+        </li>
+        <li>
+          <button
+            type="button"
+            class="border border-gray-700 bg-gray-700 text-white rounded-md px-2 py-1 transition duration-300 ease select-none hover:bg-gray-800 focus:outline-none focus:shadow-outline"
+          >
+            -
+          </button>
+        </li>
+        <li>
+          <button
+            type="button"
+            class="border border-red-500 bg-red-500 text-white rounded-md px-2 py-2 transition duration-300 ease select-none hover:bg-red-600 focus:outline-none focus:shadow-outline"
+          >
+            <svg
+              class="w-4 h-4"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 315 315"
+            >
+              <path d="m256.774,23.942h-64.836v-6.465c0-9.636-7.744-17.477-17.263-17.477h-34.348c-9.521,0-17.266,7.841-17.266,17.478v6.465h-64.835c-9.619,0-17.445,7.76-17.445,17.297v11.429c0,7.168 4.42,13.33 10.698,15.951 1.989,39.623 13.5,231.193 14.018,239.801 0.222,3.696 3.284,6.58 6.987,6.58h170.033c3.703,0 6.766-2.884 6.987-6.58 0.518-8.607 12.028-200.178 14.018-239.801 6.278-2.621 10.698-8.783 10.698-15.951v-11.43c5.68434e-14-9.537-7.826-17.297-17.446-17.297zm-119.713-6.464c0-1.918 1.465-3.478 3.266-3.478h34.348c1.8,0 3.264,1.56 3.264,3.478v6.465h-40.877v-6.465zm-82.282,23.761c0-1.818 1.546-3.297 3.445-3.297h198.549c1.899,0 3.445,1.478 3.445,3.297v11.429c0,1.819-1.546,3.299-3.445,3.299h-198.548c-1.899,0-3.445-1.479-3.445-3.299v-11.429zm181.143,259.761h-156.848c-2.055-34.247-11.479-191.674-13.51-231.033h183.867c-2.031,39.359-11.454,196.786-13.509,231.033z" />
+              <path d="m157.5,95.125c-3.866,0-7,3.134-7,7v176.109c0,3.866 3.134,7 7,7 3.866,0 7-3.134 7-7v-176.109c0-3.866-3.134-7-7-7z" />
+              <path d="m110.2,102.04c-0.202-3.86-3.507-6.837-7.355-6.625-3.86,0.201-6.827,3.494-6.625,7.355l9.182,175.829c0.195,3.736 3.285,6.635 6.984,6.635 0.123,0 0.247-0.003 0.371-0.01 3.86-0.201 6.827-3.494 6.625-7.355l-9.182-175.829z" />
+              <path d="m212.155,95.415c-3.899-0.223-7.153,2.764-7.355,6.625l-9.184,175.829c-0.202,3.861 2.765,7.154 6.625,7.355 0.125,0.007 0.248,0.01 0.371,0.01 3.698,0 6.789-2.898 6.984-6.635l9.184-175.829c0.202-3.861-2.764-7.154-6.625-7.355z" />
+            </svg>
+          </button>
+        </li>
+      </ul>
+    </div>
   );
-};
+}
 
 export default Card;
